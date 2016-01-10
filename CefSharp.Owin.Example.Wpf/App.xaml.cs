89b2651d0294194c8128fc372978f0af37d673aa
amaitland <amaitland@users.noreply.github.com>
2016-01-10 15:19:42 +1000

    Replace Owin.Builder with Microsoft.Owin - it's been updated and had over a million downloads - so presumably very well tested.

--- CefSharp.Owin.Example.Wpf/App.xaml.cs
+++ CefSharp.Owin.Example.Wpf/App.xaml.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Owin.Builder;
 using Owin;
-using Owin.Builder;
 
 namespace CefSharp.Owin.Example.Wpf
 {
